d860fa18875e42997faebb84596f8ef776513c2d
Anurag Chatterjee: My Technical Portfolio - Coding/Programming, Engineering, Innovation & Design <84841014+AnuragSChatterjee@users.noreply.github.com>
2023-03-05 23:49:57 +0800

    Add files via upload

---
+++ Climbing Stairs/climbing stairs.c
@@ -0,0 +1,22 @@
+int climbStairs(int n){
+ 
+    if (n==1) {
+        return n;
+    }
+
+    if (n==2) {
+        return n;
+    }
+
+    int most_previous_value = 1;
+    int previous_value = 1;
+    int current_value = 0;
+    int i;
+
+    for (i=2; i<n+1; i++) {
+        current_value = previous_value + most_previous_value;
+        most_previous_value = previous_value;
+        previous_value = current_value;
+    }
+    return current_value;
+}
